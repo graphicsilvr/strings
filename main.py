7e5d62f5fb1541aa9167d4916a091e935b308df8
Sylvion Finisie <sylvionud@gmail.com>
2023-02-12 12:33:57 -0500

    Update main.py
    I had to remove information

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# Do not modify these lines
-__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
-__human_name__ = 'strings'
-
-# Add your code after this line
-
 # PART 1
 # 1. Create a variable for every player that scored
 player_0 = "Ruud Gullit"
